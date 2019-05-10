cce89fe4182435c37f8139bf5b1e92c272cc647e
2INFINITY <48837503+2infinity-gh@users.noreply.github.com>
2019-05-10 16:31:47 +0200

    Update coins.py

--- electrumx/lib/coins.py
+++ electrumx/lib/coins.py
@@ -828,31 +828,7 @@ class BitcoinCZ(Coin):
     PEER_DEFAULT_PORTS = {"t": "50001", "s": "50002"}
     RPC_PORT = 29501
     REORG_LIMIT = 5000
-    BASIC_HEADER_SIZE = 180
-    HDR_V4_SIZE = 212
-    HDR_V4_HEIGHT = 6790
-    HDR_V4_START_OFFSET = HDR_V4_HEIGHT * BASIC_HEADER_SIZE
-
-    @classmethod
-    def static_header_offset(cls, height):
-        assert cls.STATIC_BLOCK_HEADERS
-        if height >= cls.HDR_V4_HEIGHT:
-            relative_v4_offset = (height - cls.HDR_V4_HEIGHT) * cls.HDR_V4_SIZE
-            return cls.HDR_V4_START_OFFSET + relative_v4_offset
-        else:
-            return height * cls.BASIC_HEADER_SIZE
-    
-    @classmethod
-    def header_hash(cls, header):
-        version, = util.unpack_le_uint32_from(header)
-        if version >= 4:
-            import lyra2cz_hash
-            return lyra2cz_hash.getPoWHash(header)
-        if version == 3:
-            import lyra2z_hash
-            return lyra2z_hash.getPoWHash(header)
-        if version == 2:
-            return sha256_hash(header)
+    BASIC_HEADER_SIZE = 80
 
 
 class Unitus(Coin):
